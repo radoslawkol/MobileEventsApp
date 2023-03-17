27644cf261df26328b567473a7f969da3a6dc367
Radosław Kołoczek <radoslaw.koloczek@gmail.com>
2023-03-17 19:46:27 +0100

    Add login api

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -9,10 +9,17 @@ export const signup = async (req: Request, res: Response) => {
 
 		const { firstName, surname, email, password } = req.body;
 
+		if (!firstName || !surname || !email || !password) {
+			return res.status(400).json({
+				status: "fail",
+				message: "Fields cannot be empty.",
+			});
+		}
+
 		const isEmailAlreadyExists = await User.findOne({ email });
 
 		if (isEmailAlreadyExists) {
-			return res.status(400).json({
+			return res.status(401).json({
 				status: "fail",
 				message:
 					"An account is already registered on this email. Try with a different one.",
@@ -42,6 +49,56 @@ export const signup = async (req: Request, res: Response) => {
 		});
 	} catch (err) {
 		console.log(err);
+		res.status(500).json({
+			status: "fail",
+			message: err,
+		});
+	}
+};
+
+export const login = async (req: Request, res: Response) => {
+	try {
+		const { email, password } = req.body;
+
+		if (!email || !password) {
+			return res.status(400).json({
+				status: "fail",
+				message: "Please provide correct email and password.",
+			});
+		}
+
+		const user = await User.findOne({ email });
+
+		if (!user) {
+			return res.status(401).json({
+				status: "fail",
+				message: "This email is not assigned to any account.",
+			});
+		}
+
+		const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+		if (!isPasswordCorrect) {
+			return res.status(401).json({
+				status: "fail",
+				message: "Invalid credentials.",
+			});
+		}
+
+		const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+
+		res.status(200).json({
+			status: "success",
+			data: {
+				user: {
+					email: user.email,
+					firstName: user.firstName,
+					surname: user.surname,
+				},
+				token,
+			},
+		});
+	} catch (err) {
 		res.status(500).json({
 			status: "fail",
 			message: err,
