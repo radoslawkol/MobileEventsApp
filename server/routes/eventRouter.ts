efa03765c8a04247093dc6566907e14821c37e39
Radosław Kołoczek <radoslaw.koloczek@gmail.com>
2023-03-18 19:10:15 +0100

    Add auth middleware

--- server/routes/eventRouter.ts
+++ server/routes/eventRouter.ts
@@ -4,10 +4,11 @@ import {
 	getEventById,
 } from "./../controllers/eventController.js";
 import express, { Router } from "express";
+import { authenticate } from "../middlewares/authenticate.js";
 
 const router: Router = express.Router();
 
-router.route("/").post(createEvent).get(getAllEvents);
+router.route("/").post(authenticate, createEvent).get(getAllEvents);
 router.route("/:id").get(getEventById);
 
 export default router;
